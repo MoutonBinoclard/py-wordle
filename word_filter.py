bd9f98600259ed6b4aa6f0a5bf9f0fdb44ef15f0
MoutonBinoclard <oscarbarral05@gmail.com>
2025-06-05 09:46:53 +0200

    filter file

---
+++ word_filter.py
@@ -0,0 +1,8 @@
+def remove_proper_noun(input_path, output_path):
+    with open(input_path, encoding="utf-8") as infile, open(output_path, "w", encoding="utf-8") as outfile:
+        for line in infile:
+            word = line.strip()
+            if word and not word[0].isupper():
+                outfile.write(word + "\n")
+
+remove_proper_noun("words_fr_beta.txt", "words_fr.txt")
